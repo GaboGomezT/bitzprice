6649e8110b8d8f973fb7db190c220f63636a6c7a
GaboGomez09 <gomezgabriel09@gmail.com>
2020-12-13 23:24:34 -0600

    created better layout

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/navbar";
+import Layout from "../components/layout";
 
 const About = () => (
-    <div>
-        <Navbar/>
-        <h1>About BitzPrices</h1>
-        <p>Application to view Bitcoin</p>
-    </div>
+    <Layout>
+        <div>
+            <h1>About BitzPrices</h1>
+            <p>Application to view Bitcoin</p>
+        </div>
+    </Layout>
 );
 
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import Navbar from "../components/navbar";
+import Layout from "../components/layout";
 
 const Index = () => (
-    <div>
-        <Navbar/>
-        <h1>Welcome to BitzPrice prices</h1>
-    </div>
+    <Layout>
+        <div>
+            <h1>Welcome to BitzPrice prices</h1>
+        </div>
+    </Layout>
 );
 
 export default Index;
